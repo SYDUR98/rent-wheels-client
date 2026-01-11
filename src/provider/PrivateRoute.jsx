34f70326a768f2d9a7b04a5813165bcda131eacf
sydur <eng.sydur@gmail.com>
2026-01-11 17:30:44 +0600

    correction in private route

--- src/provider/PrivateRoute.jsx
+++ src/provider/PrivateRoute.jsx
@@ -2,12 +2,13 @@ import React, { use } from 'react';
 import { Navigate, useLocation } from 'react-router';
 import { AuthContext } from './AuthContext';
 import LoadingSpinner from '../Coponents/LoadingSpinner/LoadingSpinner';
+import LoadingPage from '../Coponents/Shared/LoadingPage';
 
 const PrivateRoute = ({children}) => {
     const {user,loading} = use(AuthContext)
     const location = useLocation()
     if(loading){
-        return <LoadingSpinner></LoadingSpinner>
+        return <LoadingPage></LoadingPage>
     }
 
     if(user && user.email){
